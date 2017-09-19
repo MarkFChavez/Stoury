eaa6a6a1414c3c8073d7333c9d09a90200f14dc5
Mark Chavez <markchav3z@gmail.com>
2017-09-19 22:03:44 +0800

    set status bar color to light

--- src/components/screens/CatalogScreen.js
+++ src/components/screens/CatalogScreen.js
@@ -1,54 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   View,
   Text,
   ListView,
   TouchableWithoutFeedback,
-  Image
-} from 'react-native';
+  Image,
+  StatusBar
+} from 'react-native'
 import Card from '../common/Card'
-import _ from 'lodash';
-import data from '../../../Mountains.json';
+import _ from 'lodash'
+import data from '../../../Mountains.json'
 
 class CatalogScreen extends Component {
   static navigationOptions = {
     title: '△ BUNDOK.PH',
-    headerStyle: { backgroundColor: '#fff' },
+    headerStyle: { backgroundColor: '#000' },
     headerTitleStyle: {
       fontWeight: '700',
       fontFamily: 'Avenir Next',
-      letterSpacing: 1
+      letterSpacing: 1,
+      color: '#fff'
     },
-    headerRight: <Text style={{ marginRight: 10, fontSize: 12 }}> SEARCH </Text>
+    headerRight: (
+      <Text style={{ marginRight: 10, fontSize: 12, color: '#fff' }}>
+        {' '}
+        SEARCH{' '}
+      </Text>
+    )
   }
 
-  setDataSource () {
+  componentDidMount() {
+    // set statusbar to light-content
+    StatusBar.setBarStyle('light-content')
+  }
+
+  setDataSource() {
     const ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2
-    });
+    })
 
     const mountainsArray = _.map(data.mountains, (value, key) => {
       return { ...value, name: key }
-    });
+    })
 
-    this.dataSource = ds.cloneWithRows(mountainsArray);
+    this.dataSource = ds.cloneWithRows(mountainsArray)
   }
 
-  constructor (props) {
-    super(props);
-    this.setDataSource();
+  constructor(props) {
+    super(props)
+    this.setDataSource()
   }
 
-  onButtonPress (mountain) {
-    const { navigation } = this.props;
+  onButtonPress(mountain) {
+    const { navigation } = this.props
 
     // go to ItineraryScreen
     // navigation.navigate('itinerary', { itinerary: mountain });
   }
 
-  renderRow (mountain) {
-    const { name, location, image, masl  } = mountain;
-    const { container, imageStyle, textContainerStyle, titleStyle, subtitleStyle } = styles;
+  renderRow(mountain) {
+    const { name, location, image, masl } = mountain
+    const {
+      container,
+      imageStyle,
+      textContainerStyle,
+      titleStyle,
+      subtitleStyle
+    } = styles
 
     return (
       <Card>
@@ -62,7 +80,10 @@ class CatalogScreen extends Component {
 
             <View style={textContainerStyle}>
               <Text style={titleStyle}> {name.toUpperCase()} </Text>
-              <Text style={subtitleStyle}> {location.toUpperCase()} • {masl} MASL </Text>
+              <Text style={subtitleStyle}>
+                {' '}
+                {location.toUpperCase()} • {masl} MASL{' '}
+              </Text>
             </View>
           </View>
         </TouchableWithoutFeedback>
@@ -70,7 +91,7 @@ class CatalogScreen extends Component {
     )
   }
 
-  render () {
+  render() {
     return (
       <View style={{ backgroundColor: '#fff' }}>
         <ListView
@@ -109,4 +130,4 @@ const styles = {
   }
 }
 
-export default CatalogScreen;
+export default CatalogScreen
